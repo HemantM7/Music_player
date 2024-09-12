b9fd0ad351f731049f1423fcf2fab4ce9be53a50
HemantM7 <hemantmistri00@gmail.com>
2024-09-12 09:01:43 +0530

    add some fies

---
+++ musiclist.js
@@ -0,0 +1,74 @@
+let allMusic = [
+    {
+        name: "Ishq Tera", // music name 
+        artist: "Guru Randhawa", //artist name
+        img: "Ishq Tera", //image name
+        src: "Ishq Tera (RiskyjaTT.CoM)"  //music name here
+    },
+    {
+        name: "Jee Karda", // music name 
+        artist: "Garry Sandhu", //artist name
+        img: "Jee Karda", //image name
+        src: "Jee Karda - Garry Sandhu"  //music name here
+    },
+    {
+        name: "Khamoshiyan", // music name 
+        artist: "Arijit singh", //artist name
+        img: "Khamoshiyan", //image name
+        src: "Khamoshiyan - Khamoshiyan 192 kbps"  //music name here
+    },
+    {
+        name: "Kuch To Hai", // music name 
+        artist: "Armaan Malik", //artist name
+        img: "Kuch To Hai", //image name
+        src: "Kuch To Hai @ IndiaMp3.Com"  //music name here
+    },
+    {
+        name: "Main Teri Ho Gayi Sohnea", // music name 
+        artist: "Millind Gaba", //artist name
+        img: "Main Teri Ho Gayi-Sohnea", //image name
+        src: "Main Teri Ho Gayi Sohnea Cros"  //music name here
+    },
+    {
+        name: "Mueve La Cintura", // music name 
+        artist: "PitBull, Guru Randhawa", //artist name
+        img: "Mueve La Cintura", //image name
+        src: "Mueve La Cintura (RiskyjaTT.CoM)"  //music name here
+    },
+    {
+        name: "Namo Namo", // music name 
+        artist: "Amit Trivedi", //artist name
+        img: "Namo Namo", //image name
+        src: "Namo Namo (RiskyjaTT.CoM)"  //music name here
+    },
+    {
+        name: "Neendein_Khul_Jaati_Hain", // music name 
+        artist: "Meet Bros, Mika Singh", //artist name
+        img: "Neendein Khul Jaati Hain", //image name
+        src: "Neendein_Khul_Jaati_Hain_32_123Musiq.mobi"  //music name here
+    },
+    {
+        name: "Rabba Main Toh Mar Gaya Oye", // music name 
+        artist: "Shahid Mallya", //artist name
+        img: "Rabba Main Toh Mar Gaya Oye", //image name
+        src: "Rabba Main Toh Mar Gaya Oye"  //music name here
+    },
+    {
+        name: "Tera Pyar", // music name 
+        artist: "Jassi Gill", //artist name
+        img: "Tera Pyar", //image name
+        src: "Tera Pyar - RiskyjaTT.CoM"  //music name here
+    },
+    {
+        name: "Ve Maahi", // music name 
+        artist: "Arijit Singh", //artist name
+        img: "Ve Maahi", //image name
+        src: "Ve Maahi-(Mr-Jatt.com)"  //music name here
+    },
+    {
+        name: "Sanu Kehndi", // music name 
+        artist: "Arijit Singh", //artist name
+        img: "Ve Maahi", //image name
+        src: "Sanu Kehndi (Mr-Jatt.com)"  //music name here
+    },
+]
